fb44746400c4910ecee5a6781144e2b46ce2a2d9
Sachin <sachnaror@gmail.com>
2024-11-01 10:37:27 +0530

    Each day brings new lessons to embrace

--- store/views.py
+++ store/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+//Adding views for listing products, adding new ones, and purchasing products.
 
-# Create your views here.
+from django.http import JsonResponse
+
+from .utils import create_product, purchase_product
+
+
+def add_product(request):
+    name = request.GET.get("name")
+    price = int(request.GET.get("price"))
+    receipt = create_product(name, price)
+    return JsonResponse({"status": "Product added", "transaction": receipt.transactionHash.hex()})
+
+def buy_product(request):
+    product_id = int(request.GET.get("product_id"))
+    receipt = purchase_product(product_id)
+    return JsonResponse({"status": "Purchase complete", "transaction": receipt.transactionHash.hex()})
